90a3fd3e5b0a66603e09fb478509467846453d04
Troy Dawson <troyd9216@gmail.com>
2020-07-20 04:37:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,13 +80,10 @@ def adding_matz
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]
-      },
-      :yukihiro_matsumoto => {
-        :known_for => "ruby",
-        :languages => ["LISP", "C"]
       }
-     
-    }
+      
+      }
+     programmer_hash.merge(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]})
 programmer_hash[:yukihiro_matsumoto]
 end
 
