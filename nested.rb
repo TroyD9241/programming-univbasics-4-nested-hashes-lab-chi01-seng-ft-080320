11dbef2816abb8d31af665a637174217423f9379
Troy Dawson <troyd9216@gmail.com>
2020-07-20 04:29:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,4 +111,19 @@ end
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
   # return the entire updated hash
-end 
+
+	programmer_hash = 
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+end
